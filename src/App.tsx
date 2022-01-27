8fc2eefeaabfa059c882071e42ff97320fea491d
laguta-danil <laguta.d.s@gmail.com>
2022-01-27 15:07:52 +0200

    add-chat-form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './components/HomePage';
+import { ChatComponent } from './components/ChatComponent';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <ChatComponent />
     </div>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
+import messagesSlice from './messagesSlice';
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counterSlice'
 
 export const store = configureStore({
   reducer: {
-      counter: counterSlice
+      counter: counterSlice,
+      ms: messagesSlice
   },
 })
 

240f1534e3f59cd5b35591a344bcc990b52edd69
laguta-danil <laguta.d.s@gmail.com>
2022-01-27 10:44:44 +0200

    initialization TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <div>
-        Chat
-      </div>
+      <HomePage />
     </div>
   );
 }

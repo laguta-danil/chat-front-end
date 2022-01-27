06d9e217beb8800da51024cb48cf38624af75029
RinaOcean <70663656+RinaOcean@users.noreply.github.com>
2022-01-27 13:32:23 +0200

    add-markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './components/HomePage';
+import { RegLog } from './components/RegistrationLogin';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <RegLog />
     </div>
   );
 }

2544aa3d24790dfbcc954b75a45a311fed2e68a7
Daria <daria.zhuravlova23@gmail.com>
2022-01-28 14:52:06 +0200

    add get chats thunk

--- src/redux/chatSlice.ts
+++ src/redux/chatSlice.ts
@@ -1,46 +1,81 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import axios from "axios";
 
 export interface ChatState {
-	title: string
-	users: Array<string>
-  messages: Array<any>
-	id: string
+  name: string;
+  users: Array<string>;
+  messages: Array<any>;
+  _id: string;
 }
 
 const initialState: Array<ChatState> = [
-	{
-		title: 'Chat 1',
-		users: [],
-    messages: [],
-		id: '1',
-	},
-	{
-		title: 'Chat 2',
-		users: [],
-    messages: [],
-		id: '2',
-	},
-	{
-		title: 'Chat 3',
-		users: [],
-    messages: [],
-		id: '3',
-	},
-]
+  
+];
+
+export const fetchChats = createAsyncThunk(
+  "chats/fetchAll",
+  async (thunkAPI) => {
+    try {
+      const res: any = await axios.get(`http://localhost:5000/chats`);
+      const data = res.data;
+      return data;
+    } catch (e: any) {
+      return e;
+    }
+  }
+);
+
+// export const addNewChat = createAsyncThunk(
+//   "chats/addNewChat",
+//   async (chatData: any, thunkAPI) =>{
+//     try {
+//       const res: any = await axios.post(`http://localhost:5000/chats`, chatData );
+//           const data = res.data;
+//           console.log(data);
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+
+// )
+
+// export const deleteChatById = createAsyncThunk(
+//   "chats/deleteChatById",
+//   async (thunkAPI) => {
+//     try {
+//       const res: any = await axios.delete(`http://localhost:5000/chats/:id`)
+//     } catch (error) {
+//       console.log(error)
+//     }
+//   }
+// )
 
 export const chatSlice = createSlice({
-	name: 'chat',
-	initialState,
-	reducers: {
-		addChat: (state: any, action) => {
-			return [...state, { ...action.payload, id: 0, users: [] }]
-		},
-		deleteChat: (state: any, action) => {
-			return state.filter((item: any) => item.id != action.payload)
-		},
-	},
-})
+  name: "chats",
+  initialState,
+  reducers: {
+    addChat: (state: any, action) => {
+      return [...state, { ...action.payload, id: 0, users: [] }];
+    },
+    deleteChat: (state: any, action) => {
+      return state.filter((item: any) => item.id != action.payload);
+    },
+  },
+  extraReducers: {
+    [fetchChats.fulfilled.type]: (state, action) => {
+      return state = action.payload;
+    },
+    [fetchChats.pending.type]: (state) => {},
+    [fetchChats.rejected.type]: (state, action: PayloadAction<string>) => {},
+    // [addNewChat.fulfilled.type]: (state, action) => {
+    //   return [...state, action.payload];
+    // },
+    // [addNewChat.pending.type]: (state) => {},
+    // [addNewChat.rejected.type]: (state, action: PayloadAction<string>) => {},
+  },
+  
+});
 
-export const { addChat, deleteChat } = chatSlice.actions
+export const { addChat, deleteChat } = chatSlice.actions;
 
-export default chatSlice.reducer
+export default chatSlice.reducer;
